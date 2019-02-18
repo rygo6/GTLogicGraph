3f826dbe1fca05f51a01cec110624044e1f732c5
rygo6 <rygo6@me.com>
2019-02-17 20:06:11 -0600

    colored input and output nodes

--- Scripts/Editor/Nodes/LogicNodeView.cs
+++ Scripts/Editor/Nodes/LogicNodeView.cs
@@ -28,7 +28,12 @@ namespace GeoTetra.GTLogicGraph
         {
             AddStyleSheetPath("Styles/LogicNodeView");
 
-            _connectorListener = connectorListener;
+            if (logicNodeEditor is IInputNode)
+                AddToClassList("input");
+            else if (logicNodeEditor is IOutputNode)
+                AddToClassList("output");
+
+                _connectorListener = connectorListener;
             LogicNodeEditor = logicNodeEditor;
             title = LogicNodeEditor.NodeType();
 
@@ -55,7 +60,7 @@ namespace GeoTetra.GTLogicGraph
                 }
             }
             contents.Add(controlsContainer);
-            
+
             // Add port input container, which acts as a pixel cache for all port inputs
             _portInputContainer = new VisualElement
             {
@@ -67,7 +72,7 @@ namespace GeoTetra.GTLogicGraph
 
             List<PortDescription> foundSlots = new List<PortDescription>();
             logicNodeEditor.GetSlots(foundSlots);
-            AddSlots(foundSlots);
+            AddPorts(foundSlots);
 
             SetPosition(new Rect(logicNodeEditor.Position.x, logicNodeEditor.Position.y, 0, 0));
             UpdatePortInputs();
@@ -76,7 +81,7 @@ namespace GeoTetra.GTLogicGraph
             UpdatePortInputVisibilities();
         }
 
-        private void AddSlots(IEnumerable<PortDescription> slots)
+        private void AddPorts(IEnumerable<PortDescription> slots)
         {
             foreach (var slot in slots)
             {
@@ -87,7 +92,7 @@ namespace GeoTetra.GTLogicGraph
                     inputContainer.Add(port);
             }
         }
-        
+
         public override bool expanded
         {
             get { return base.expanded; }
@@ -110,9 +115,10 @@ namespace GeoTetra.GTLogicGraph
             {
                 if (!_portInputContainer.OfType<PortInputView>().Any(a => Equals(a.Description, port.Description)))
                 {
-                    var portInputView = new PortInputView(port.Description) { style = { positionType = PositionType.Absolute } };
+                    var portInputView = new PortInputView(port.Description)
+                        {style = {positionType = PositionType.Absolute}};
                     _portInputContainer.Add(portInputView);
-                    port.RegisterCallback<GeometryChangedEvent>(evt => UpdatePortInput((LogicPort)evt.target));
+                    port.RegisterCallback<GeometryChangedEvent>(evt => UpdatePortInput((LogicPort) evt.target));
                 }
             }
         }
@@ -120,10 +126,12 @@ namespace GeoTetra.GTLogicGraph
         private void UpdatePortInput(LogicPort port)
         {
             Debug.Log("UpdatePortInput");
-            
-            var inputView = _portInputContainer.OfType<PortInputView>().First(x => Equals(x.Description, port.Description));
 
-            var currentRect = new Rect(inputView.style.positionLeft, inputView.style.positionTop, inputView.style.width, inputView.style.height);
+            var inputView = _portInputContainer.OfType<PortInputView>()
+                .First(x => Equals(x.Description, port.Description));
+
+            var currentRect = new Rect(inputView.style.positionLeft, inputView.style.positionTop, inputView.style.width,
+                inputView.style.height);
             var targetRect = new Rect(0.0f, 0.0f, port.layout.width, port.layout.height);
             targetRect = port.ChangeCoordinatesTo(inputView.shadow.parent, targetRect);
             var centerY = targetRect.center.y;
@@ -147,13 +155,14 @@ namespace GeoTetra.GTLogicGraph
                 _foundEdges.Clear();
                 LogicNodeEditor.Owner.LogicGraphEditorObject.LogicGraphData.GetEdges(
                     LogicNodeEditor.NodeGuid,
-                    portInputView.Description._memberName, 
+                    portInputView.Description._memberName,
                     _foundEdges);
                 portInputView.visible = expanded && _foundEdges.Count == 0;
                 if (portInputView.visible != oldVisibility)
                     _portInputContainer.MarkDirtyRepaint();
             }
         }
+
         private readonly List<SerializedEdge> _foundEdges = new List<SerializedEdge>();
 
         public void UpdatePortInputTypes()
