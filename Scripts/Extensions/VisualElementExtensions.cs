47e3be2af1fd2f6559f5a2000708532d1240db89
rygo6 <rygo6@me.com>
2019-05-18 16:44:31 -0500

    moved extensions class to proper directory

---
+++ Scripts/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace GeoTetra.GTLogicGraph.Extensions
+{
+    public static class VisualElementExtensions
+    {
+        public static void LoadAndAddStyleSheet(this VisualElement visualElement, string sheetPath)
+        {
+            StyleSheet styleSheet = Resources.Load(sheetPath, typeof(StyleSheet)) as StyleSheet;
+            if ((UnityEngine.Object) styleSheet == (UnityEngine.Object) null)
+                Debug.LogWarning((object) string.Format("Style sheet not found for path \"{0}\"", (object) sheetPath));
+            else
+                visualElement.styleSheets.Add(styleSheet);
+        }
+    }
+}
